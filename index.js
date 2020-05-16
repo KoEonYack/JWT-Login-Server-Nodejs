96f2dab56e48cc425dab77680ac802b958de60da
KoEonYack <21300035@handong.edu>
2020-05-16 18:13:51 +0900

    DB connection

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express()
+const port = 5002;
+
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// applicaton/x-www-form-urlencoded 를 분석해서 가져올 수 있게
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json 을 분석해서 가져올 수 있게
+app.use(bodyParser.json());
+
 
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://covenant:1234@cluster0-nfvbl.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://apple:1234@cluster0-nfvbl.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err))
 
-const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) =>
     res.send('HelloWorld')
 )
+
+app.post("/register", (req, res) => {
+
+    const user = new User(req.body) 
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
