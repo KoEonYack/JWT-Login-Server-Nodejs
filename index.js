11b5ed6d1b785d69693336ed2518b6f3b933929e
KoEonYack <21300035@handong.edu>
2020-05-16 18:35:16 +0900

    Mongo db url을 별도 파일로 분리

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express()
 const port = 5002;
-
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 // applicaton/x-www-form-urlencoded 를 분석해서 가져올 수 있게
@@ -11,9 +13,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 // application/json 을 분석해서 가져올 수 있게
 app.use(bodyParser.json());
 
-
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://apple:1234@cluster0-nfvbl.mongodb.net/test?retryWrites=true&w=majority', {
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err))
